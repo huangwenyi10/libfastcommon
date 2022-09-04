5e3d0f84154fb66c77d0f82d116873bef401d762
ay <huangwenyi@anta.com>
2022-09-04 21:15:16 +0800

    20220904-remark

--- src/sockopt.c
+++ src/sockopt.c
@@ -1971,8 +1971,22 @@ int tcpsetserveropt(int fd, const int timeout)
 	linger.l_linger = timeout;
 #endif
 */
-	linger.l_onoff = 0;
+    /**
+     * linger.l_onoff = 0;
 	linger.l_linger = 0;
+    SO_DEBUG 打开或关闭排错模式
+    SO_REUSEADDR 允许在bind ()过程中本地地址可重复使用
+    SO_TYPE 返回socket 形态.
+    SO_ERROR 返回socket 已发生的错误原因
+    SO_DONTROUTE 送出的数据包不要利用路由设备来传输.
+    SO_BROADCAST 使用广播方式传送
+    SO_SNDBUF 设置送出的暂存区大小
+    SO_RCVBUF 设置接收的暂存区大小
+    SO_KEEPALIVE 定期确定连线是否已终止.
+    SO_OOBINLINE 当接收到OOB 数据时会马上送至标准输入设备
+    SO_LINGER 确保数据安全且可靠的传送出去.
+    https://blog.csdn.net/zhangkuo8512067/article/details/43984243
+     */
 	if (setsockopt(fd, SOL_SOCKET, SO_LINGER, \
                 &linger, (socklen_t)sizeof(struct linger)) < 0)
 	{
